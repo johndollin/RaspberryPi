14aeea7d4cbb3282a6a183e85ed064fccc3b199c
johndollin <>
2015-11-17 08:50:11 +0000

    First commit.

---
+++ helloworld/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jdollin
+ *
+ * Created on 14 November 2015, 23:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World!\n");
+	return (EXIT_SUCCESS);
+}
+
